99cbf9b68c54724309218c495e7a9000a46341c8
CN-Tower <247114045@qq.com>
2017-11-30 17:06:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,42 +2,38 @@ var fs = require('fs');
 var proxyPath = 'proxy.conf.js';
 var isDev = process.argv[2] && process.argv[2] === 'dev';
 
-if (!isDev) {
-  createMockProxyServer();
-} else {
-  readProxyConfig();
-}
+isDev ? readProxyConfig() : createMockProxyServer(true);
+
 /**
- * 读取代理设置
+ * 获取http代理配置
  ***************************************************************************/
 function readProxyConfig() {
-  var isUseMockProxy, isUseDynamicProxy;
+  var isUseMockProxy, isEnableNDPS;
   fs.readFile(proxyPath, 'utf8', function (err, proxyData) {
     if(err) throw err;
     proxyData = proxyData.replace(/\s*/mg, '');
-    isUseDynamicProxy = proxyData.split('useDynamicProxy=')[1].substr(0, 4) === 'true';
+    isEnableNDPS = proxyData.split('isEnableNDPS=')[1].substr(0, 4) === 'true';
     isUseMockProxy = proxyData.split('proxies[')[1].substr(0, 1) === '0';
 
-    if (isUseDynamicProxy) {
+    if (isEnableNDPS) {
       createDynamicProxyServer();
     } else if (isUseMockProxy) {
-      createMockProxyServer();
+      createMockProxyServer(false);
     }
   });
-}
-
-
-/**
- * 启动桩服务器
- ***************************************************************************/
-function createMockProxyServer() {
-  require('./mock_server')();
 }
 
 /**
  * 启动动态代理服务器
  ***************************************************************************/
 function createDynamicProxyServer() {
-  require('./dynamic_proxy')();
+  require('./ndps')();
+}
+
+/**
+ * 启动桩服务器
+ ***************************************************************************/
+function createMockProxyServer(isShowBar) {
+  require('./mock')(isShowBar);
 }
 
