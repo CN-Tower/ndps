4b24d056c3123f1849969c4d33bc4a7e8f89e2b0
CN-Tower <cntower@yahoo.com>
2018-07-18 17:18:21 +0800

    Update ndps.js

--- src/ndps.js
+++ src/ndps.js
@@ -1,200 +1,145 @@
-module['exports'] = function (opt) {
-  var fs = require('fs');
-  var http = require('http');
-  var httpProxy = require('http-proxy');
-  var valueLooker = require('value-looker');
-  var ProgressBar = require('progress');
+const fs = require('fs');
+const fn = require('funclib');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-  var proxies, proxyStr;
-  var proxyServer, httpServer;
-  var progressTimer, progressBar, tickInterval;
-  var port, proxyConfPath, proxiesAnchor, proxyIdxAnchor, isExistMockServer, mockServerPath, mockServerIdx;
+let proxies, proxyIdx, proxyStr, proxyServer, httpServer, proxyErrFlag
+  , port, proxyConfPath, proxiesAnchor, proxyIdxAnchor, onChangeProxy;
 
-  var progressHints = {
-    create: 'Creating new Proxy Server',
-    close: 'Stopping old Proxy Server'
-  };
+let isSysOnInit = true;
+let isSysAvailable = true;
+let sockets = [];
 
-  var isMockStarted = false;
-  var isSysAfterInit = false;
-  var isSysAvailable = true;
-
-  var sockets = [];
-
-
-  if(opt && opt.hasOwnProperty('ndpsPort') && opt.ndpsPort) {
-    port = opt.ndpsPort;
-  } else {
-    throw new Error('NDPS port error!');
-  }
-  if(opt && opt.hasOwnProperty('proxyConfPath') && opt.proxyConfPath) {
-    proxyConfPath = opt.proxyConfPath;
-  } else {
-    throw new Error('Proxy Conf path error!');
-  }
-  if(opt && opt.hasOwnProperty('proxiesAnchor') && opt.proxiesAnchor) {
-    proxiesAnchor = opt.proxiesAnchor;
-  } else {
-    throw new Error('Proxies Anchor error!');
-  }
-  if(opt && opt.hasOwnProperty('proxyIdxAnchor') && opt.proxyIdxAnchor) {
-    proxyIdxAnchor = opt.proxyIdxAnchor;
-  } else {
-    throw new Error('proxy Index Anchor error!');
-  }
-
+module['exports'] = opt => {
+  getNdpsConfig(opt);
   getProxyConfig();
+  initProxyServer();
   watchProxyConfigFile();
+}
 
-  /**
-   * 监听代理配置文件(proxy.conf.js)
-   ***************************************************************************/
-  function watchProxyConfigFile() {
-    fs.watchFile('./proxy.conf.js', {persistent: true, interval: 500}, function(status) {
-      if (status && isSysAvailable) {
-        getProxyConfig();
-      }
-    });
+function getNdpsConfig(opt) {
+  const getOrRaise = (path, errMsg) => {
+    if (fn.pick(opt, path)) {
+      return fn.pick(opt, path);
+    } else {
+      throw new Error(errMsg);
+    }
   }
+  port = getOrRaise('/ndpsPort', 'NDPS port error!');
+  proxyConfPath = getOrRaise('/proxyConfPath', 'Proxy Conf path error!');
+  proxiesAnchor = getOrRaise('/proxiesAnchor', 'Proxies Anchor error!');
+  proxyIdxAnchor = getOrRaise('/proxyIdxAnchor', 'proxy Index Anchor error!');
+  onChangeProxy = fn.pick(opt, '/onChangeProxy', 'fun');
+}
 
-  /**
-   * 获取具体代理配置
-   ***************************************************************************/
-  function getProxyConfig() {
-    isSysAvailable = false;
-
-    if (isSysAfterInit) valueLooker('Checking changes...', {title: 'Msg From NDPS', theme: 'verbose'});
-
-    fs.readFile(proxyConfPath, 'utf8', function (err, proxyData) {
-      if(err) throw err;
-
-      proxyData = proxyData.replace(/\s*/mg, '');
-
-      var proxyIdx = parseInt(proxyData.split(proxyIdxAnchor)[1].substr(0, 3));
-
-      eval(proxyData.split(proxiesAnchor)[1]);
-
-      if (!proxies.hasOwnProperty(proxyIdx)) {
-        var proxyErrHint = 'Proxy Index error, please select again!';
-        if (!isSysAfterInit) throw new Error(proxyErrHint);
-        isSysAvailable = true;
-        valueLooker(proxyErrHint, {title: 'Msg From NDPS', theme: 'error'});
-
-      } else if (proxies[proxyIdx] === proxyStr) {
-        isSysAvailable = true;
-        valueLooker('No changes were detected!', {title: 'Msg From NDPS', theme: 'warn'});
-
-      } else {
-        proxyStr = proxies[proxyIdx];
-        isExistMockServer = opt && opt.hasOwnProperty('mockServerPath') && opt.mockServerPath;
-        mockServerPath = isExistMockServer ? opt.mockServerPath : '';
-        mockServerIdx = isExistMockServer && opt.hasOwnProperty('mockServerIdx') ? opt.mockServerIdx : NaN;
-        if (isExistMockServer && proxyIdx === mockServerIdx && !isMockStarted) {
-          isMockStarted = true;
-          require(mockServerPath)(isSysAfterInit, function () {
-            httpServer ? closeHttpProxyServer() : initHttpProxyServer();
-          });
-        } else {
-          httpServer ? closeHttpProxyServer() : initHttpProxyServer();
-        }
-      }
-    });
+function getProxyConfig() {
+  if (!isSysOnInit) {
+    fn.log('Checking changes...', { title: 'Msg From NDPS', color: 'cyan' });
   }
-
-  /**
-   * 创建代理服务器
-   ***************************************************************************/
-  function initHttpProxyServer() {
-    if (isSysAfterInit) processProgressBar('start', progressHints['create']);
-
-    proxyServer = httpProxy.createProxyServer({target: proxyStr});
-
-    proxyServer.on('error', function(){
+  proxyErrFlag = false;
+  isSysAvailable = false;
+  const proxyData = fn.rd(proxyConfPath).replace(/\s*/mg, '');
+  try {
+    eval(`proxies = ${proxyData.split(proxiesAnchor)[1]}`);
+  } catch (e) {
+    const errHint = 'Proxy Config Error, please make sure the proxy config correct!';
+    if (isSysOnInit) {
+      throw new Error(errHint);
+    } else {
+      proxyErrFlag = true;
       isSysAvailable = true;
-      valueLooker('Proxy Server error!', {title: 'Msg From NDPS', theme: 'error'});
-    });
+      fn.log(errHint, { title: 'Msg From NDPS', color: 'red' });
+    }
+  }
+  proxyIdx = parseInt(proxyData.split(proxyIdxAnchor)[1].substr(0, 3));
+}
 
-    httpServer = http.createServer(function (request, response) {
-      proxyServer.web(request, response);
-    });
-
-    sockets = [];
-    httpServer.on("connection",function(socket){
-      sockets.push(socket);
-      socket.once("close",function(){
-        sockets.splice(sockets.indexOf(socket), 1);
-      });
-    });
-
-    httpServer.listen(port, function () {
-      if (isSysAfterInit) {
-        processProgressBar('stop', progressHints['create'], function() {
-          isSysAvailable = true;
-          valueLooker('The new proxy target is: ' + proxyStr, {title: 'Msg From NDPS', theme: 'verbose'});
-        });
+function initProxyServer() {
+  if (proxyErrFlag) return;
+  if (!proxies.hasOwnProperty(proxyIdx)) {
+    const errHint = 'Proxy Index error, please select again!';
+    if (isSysOnInit) {
+      throw new Error(errHint);
+    } else {
+      isSysAvailable = true;
+      fn.log(errHint, { title: 'Msg From NDPS', color: 'red' });
+    }
+  }
+  else if (proxies[proxyIdx] === proxyStr) {
+    isSysAvailable = true;
+    fn.log('No changes were detected!', { title: 'Msg From NDPS', color: 'yellow' });
+  }
+  else {
+    proxyStr = proxies[proxyIdx];
+    if (onChangeProxy) {
+      const ndpsInfo = { isInit: isSysOnInit, proxyTarget: proxyStr };
+      if (onChangeProxy.length === 2) {
+        onChangeProxy(ndpsInfo, () => startProxyServer());
       } else {
-        isSysAfterInit = true;
-        isSysAvailable = true;
-        var ndpsStartedMsg = 'The NDPS(Node-Dynamic-Proxy-Server) is started!\nThe http-proxy target is: ' + proxyStr;
-        valueLooker(ndpsStartedMsg, {title: 'Msg From NDPS', theme: 'verbose'});
+        onChangeProxy(ndpsInfo);
+        startProxyServer();
       }
-    });
+    } else {
+      startProxyServer();
+    }
   }
+}
 
-  /**
-   * 关闭代理服务器
-   ***************************************************************************/
-  function closeHttpProxyServer() {
-    processProgressBar('start', progressHints['close']);
+function startProxyServer() {
+  httpServer ? reCreateProxyServer() : createProxyServer();
+}
 
-    sockets.forEach(function(socket){
-      socket.destroy();
+function createProxyServer() {
+  if (!isSysOnInit) {
+    fn.progress.start({ title: 'Creating new Proxy Server', width: 33 });
+  }
+  sockets = [];
+  proxyServer = httpProxy.createProxyServer({ target: proxyStr });
+  proxyServer.on('error', function () {
+    isSysAvailable = true;
+    fn.log('Proxy Server error!', { title: 'Msg From NDPS', color: 'red' });
+  });
+  httpServer = http.createServer(function (request, response) {
+    proxyServer.web(request, response);
+  });
+  httpServer.on("connection", function (socket) {
+    sockets.push(socket);
+    socket.once("close", function () {
+      sockets.splice(sockets.indexOf(socket), 1);
     });
-
-    proxyServer.close();
-
-    setTimeout(function(){
-      httpServer.close(function() {
-        processProgressBar('stop', progressHints['close'], function () {
-          initHttpProxyServer();
+  });
+  httpServer.listen(port, function () {
+    if (isSysOnInit) {
+      isSysOnInit = false;
+      isSysAvailable = true;
+      fn.log(`The NDPS(Node-Dynamic-Proxy-Server) is started!\nThe http-proxy target is: ${proxyStr}`, {
+        title: 'Msg From NDPS', color: 'cyan'
+      });
+    } else {
+      fn.progress.stop(() => {
+        isSysAvailable = true;
+        fn.log(`The new proxy target is: ${proxyStr}`, {
+          title: 'Msg From NDPS', color: 'cyan'
         });
       });
-    }, 1000);
-  }
-
-  /**
-   * 更换代理时的进度条
-   ***************************************************************************/
-  function processProgressBar(status, progressMsg, onStopped) {
-    if (status === 'start') {
-      progressBar = new ProgressBar(progressMsg + ' [:bar] :percent', {
-        complete: '=',
-        incomplete: ' ',
-        width: 31,
-        total: 20
-      });
-      clearTimeout(progressTimer);
-      tickInterval = 250;
-      tickFun('+');
     }
-    if (status === 'stop') {
-      clearTimeout(progressTimer);
-      tickInterval = 600;
-      tickFun('-');
-    }
+  });
+}
 
-    function tickFun(type) {
-      progressTimer = setTimeout(function () {
-        progressBar.tick();
-        if (type === '+') tickInterval += 300;
-        if (type === '-') tickInterval -= tickInterval * 0.25;
+function reCreateProxyServer() {
+  fn.progress.start({ title: 'Stopping old Proxy Server', width: 33 });
+  sockets.forEach(socket => socket.destroy());
+  proxyServer.close();
+  fn.timeout(1000, () => {
+    httpServer.close(() => fn.progress.stop(() => createProxyServer()));
+  });
+}
 
-        if (progressBar.complete && status === 'stop') {
-          onStopped();
-        } else {
-          tickFun(type);
-        }
-      }, tickInterval);
+function watchProxyConfigFile() {
+  fs.watchFile('./proxy.conf.js', { persistent: true, interval: 500 }, status => {
+    if (status && isSysAvailable) {
+      getProxyConfig();
+      initProxyServer();
     }
-  }
-};
+  });
+}
